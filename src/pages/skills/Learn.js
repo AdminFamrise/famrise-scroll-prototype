4c13afa0a625275d721fa54ac23d45495dbabd25
AdminFamrise <info@famrise.eu>
2025-04-16 13:55:08 +0200

    Update Learn.js

--- src/pages/skills/Learn.js
+++ src/pages/skills/Learn.js
@@ -1,8 +1,8 @@
 // src/pages/skills/Learn.js
 
 import React from "react";
-import { Card, CardContent } from "../../../components/ui/Card";
-import { Button } from "../../../components/ui/Button";
+import { Card, CardContent } from "../../components/ui/Card";
+import { Button } from "../../components/ui/Button";
 import { useNavigate } from "react-router-dom";
 
 // Example: small theory modules + optional quiz placeholders
