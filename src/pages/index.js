3824cc160bc148d5dbe05877650fb4a572923c98
AdminFamrise <info@famrise.eu>
2025-04-16 15:11:35 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
-
 export default function IndexPage() {
   return (
     <div className="bg-white text-gray-800 font-sans">
