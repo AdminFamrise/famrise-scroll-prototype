6bef8396a2758d54ad59135537e4893fa8cde918
AdminFamrise <info@famrise.eu>
2025-04-16 11:14:52 +0200

    Update and rename src/components/dashboard/UserDropdown.js to src/components/dashboard/CustomerDashboard/UserDropdown.js

--- src/components/dashboard/CustomerDashboard/UserDropdown.js
+++ src/components/dashboard/CustomerDashboard/UserDropdown.js
@@ -1,4 +1,4 @@
-// src/components/dashboard/UserDropdown.js
+// src/components/dashboard/CustomerDashboard/UserDropdown.js
 
 import React, { useState } from "react";
 import { Button } from "../ui/Button";
