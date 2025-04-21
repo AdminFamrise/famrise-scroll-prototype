4882681f59fef2d7541ed6ac1bcb0b78c3e19abb
AdminFamrise <info@famrise.eu>
2025-04-21 14:03:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import React from 'react'; import ReactDOM from 'react-dom/client';
+import './index.css'; import App from './App' import initAgents from
+' . /agents/
+framework/initAgents';
+// Initialize agent framework
+initAgents ( );
+const root =
+ReactDOM. createRoot (document.getElemen tById ('root '));
+root. render (
+< React. StrictMode>
+<App />
+</React. StrictMode>
+) ;...
