27df462c6e7e58b4801a328aa97155fd3b9c720d
AdminFamrise <info@famrise.eu>
2025-04-21 14:33:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import React from 'react'; import ReactDOM from 'react-dom/client';
-import './index.css'; import App from './App' import initAgents from
+import './index.css'; import App from './App' import initAgent from
 ' . /agents/
 framework/initAgents';
 // Initialize agent framework
