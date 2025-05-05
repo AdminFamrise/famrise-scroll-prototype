f7b30236c9103871264da7220d8517a0403f08eb
AdminFamrise <info@famrise.eu>
2025-05-05 12:26:56 +0200

    Update scenario.js

--- src/pages/scenario.js
+++ src/pages/scenario.js
@@ -1,8 +1,8 @@
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, CardContent } from "../../components/ui/Card";
-import { Input } from "../../components/ui/Input";
-import { Button } from "../../components/ui/Button";
+import { Card, CardContent } from "../components/ui/Card";
+import { Input } from "../components/ui/Input";
+import { Button } from "../components/ui/Button";
 import { mergeUserData } from "../services/UserDataService";
 
 const ScenarioPage = () => {
